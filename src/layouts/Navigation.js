56474ed6fd6fe9e8810f92a7facf047ce39463ee
aleksander Micyk <aleksander.micyk@gmail.com>
2022-10-05 22:04:00 +0200

    create navigation panel

--- src/layouts/Navigation.js
+++ src/layouts/Navigation.js
@@ -1,8 +1,26 @@
 import React from 'react';
+import {NavLink} from 'react-router-dom'
+import "../styles/Navigation.css"
+
+const list = [
+    {name: "start", path: "/", },
+    {name: "produkty", path: "/products"},
+    {name: "kontakt", path: "/contact"},
+    {name: "panel admina", path: "/admin"},
+]
 
 const Navigation = () => {
+    const menu = list.map(item => (
+        <li key={item.name}>
+            <NavLink to={item.path} end>{item.name} </NavLink>
+        </li>
+    ))
     return ( 
-        <div>Navigation</div>
+        <nav>
+            <ul>
+                {menu}
+            </ul>
+        </nav>
     );
 }
  
