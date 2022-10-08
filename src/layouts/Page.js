a1222d36c999816790698a0c125c3187a5b31a87
aleksander Micyk <aleksander.micyk@gmail.com>
2022-10-08 18:39:43 +0200

    add routing on the pages

--- src/layouts/Page.js
+++ src/layouts/Page.js
@@ -1,9 +1,21 @@
 import React from 'react';
+import {Routes, Route} from 'react-router-dom'
+import HomePage from '../pages/HomePage'
+import ProductPage from '../pages/ProductPage'
+import ContactPage from '../pages/ContactPage'
+import AdminPage from '../pages/AdminPage'
+import ErrorPage from '../pages/ErrorPage'
 
 const Page = () => {
     return ( 
-        <div>Page</div>
-    );
+        <Routes>
+            <Route path="/"  element={<HomePage/>} />
+            <Route path="/contact"  element={<ContactPage/>} />
+            <Route path="/products"  element={<ProductPage/>} />
+            <Route path="/admin"  element={<AdminPage/>} />
+            <Route path="*"  element={<ErrorPage/>} />
+        </Routes>
+    )
 }
  
 export default Page;
